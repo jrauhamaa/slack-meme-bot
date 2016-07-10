adea1e2ec9e8fb47996a44311c58f9289936bc23
johannes.rauhamaa@iki.fi <Johannes Rauhamaa>
2016-07-10 13:54:37 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Slack meme bot
+
+## How to use
+populate source_images-directory with source images. Update image_dict in app.py accordingly.
+
+`export FLASK_APP=app.py`
+`flask run`
---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify, request, json, send_from_directory
+from memegenerator import make_meme
+
+app = Flask(__name__)
+
+source_images_path = "source_images"
+image_dict = {
+    "doge": "doge.jpg"
+}
+
+
+@app.route('/', methods=['POST'])
+def hello_world():
+    data = json.loads(request.data)
+    # TODO: support the actual slack request format
+    message = data.get("message")
+    if not message:
+        return '', 400
+    # Message format is expected to be "[name] [top text]/[bottom text]" or "[name] [bottom text]"
+    try:
+        [name, content] = message.split(" ", 1)
+    except ValueError:
+        return '', 400  # TODO: return instructions on how to use the bot
+    source_image_name = image_dict.get(name)
+    if not source_image_name:
+        return '', 400  # TODO: tell the user that the image wasn't found
+
+    image_path = "{path}/{name}".format(path=source_images_path, name=source_image_name)
+    if len(content.split("/")) == 1:
+        top_text = ""
+        bottom_text = content
+    else:
+        [top_text, bottom_text] = content.split("/", 1)
+    bottom_text = bottom_text.strip()
+    top_text = top_text.strip()
+
+    filename = make_meme(top_text, bottom_text, image_path)
+
+    response = {
+        "response_type": "in_channel",
+        "text": filename
+    }
+    return jsonify(response)
+
+
+@app.route('/images/<filename>')
+def send_image(filename):
+    return send_from_directory('images', filename)
