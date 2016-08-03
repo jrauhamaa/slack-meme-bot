8911a40735ab9bf23f642d698794cb99c6e76d74
johannes.rauhamaa@iki.fi <Johannes Rauhamaa>
2016-08-03 20:30:01 +0300

    Code cleanup. Move configs to config.py

--- app.py
+++ app.py
@@ -1,40 +1,97 @@
-from flask import Flask, jsonify, request, json, send_from_directory, redirect
-from memegenerator import make_meme
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
-from werkzeug.utils import secure_filename
 import os
 from uuid import uuid4
 import hashlib
 
-cloudinary.config(
-  cloud_name = "polirytmi",
-  api_key = "457765985322576",
-  api_secret = "JX4FiBiLVx5GdRVTGPXqU-RxiYI"
-)
+from flask import Flask, jsonify, request, json, send_from_directory, redirect
+import cloudinary
+import cloudinary.api
+import cloudinary.uploader
+
+from memegenerator import make_meme
+
+import config
+
 
 app = Flask(__name__)
+cloudinary.config(**config.CLOUDINARY_CONF)
 
-SOURCE_IMAGES_PATH = "source_images"
-UPLOAD_FOLDER = 'source_images'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-SERVER_SECRET = "DUMMYSECRET"
-HASH_LENGTH = 14
 
 def encode_id(name):
-    hash = hashlib.sha256(SERVER_SECRET)
+    hash = hashlib.sha256(config.SERVER_SECRET)
     hash.update(name)
-    public_id_hash = hash.hexdigest()[0:HASH_LENGTH]
+    public_id_hash = hash.hexdigest()[0:config.HASH_LENGTH]
     public_id = "-".join([name, public_id_hash])
     return public_id
+
 
 def decode_id(public_id):
     return public_id.rsplit("-", 1)[0]
 
+
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1] in config.ALLOWED_EXTENSIONS
+
+
+def list_source_images():
+    images_all_data = cloudinary.api.resources(
+        type="upload",
+        prefix=config.SOURCE_IMAGES_PATH
+    )
+    resources = images_all_data["resources"]
+
+    def formatResource(resource):
+        base_string = "http://res.cloudinary.com/polirytmi/image/upload"
+        quality = "q_60"
+        image_id = "{id}.{format}".format(
+            id=resource["public_id"],
+            format=resource["format"]
+        )
+        thumbnail_url = "{base}/{quality}/{image_id}".format(
+            base=base_string,
+            quality=quality,
+            image_id=image_id
+        )
+        name = resource["public_id"].split("/")[-1]
+        return {"title": decode_id(name), "thumb_url": thumbnail_url}
+    return map(formatResource, resources)
+
+
+def create_meme(name, top_text, bottom_text):
+    source_images = list_source_images()
+    source_image_names = map(lambda i: i.get("title"), source_images)
+    # Check that such an image exists, respond with error if not
+    if name not in source_image_names:
+        image_not_found_text = "The image [{name}] was not found." \
+            " Here's a list of valid images:".format(name=name)
+        response = {
+            "text": image_not_found_text,
+            "attachments": source_images
+        }
+        return jsonify(response), 200
+
+    # Find correct download url
+    source_image = source_images[source_image_names.index(name)]
+    image_url = source_image["thumb_url"]
+
+    # Make meme to local file
+    filename = make_meme(top_text, bottom_text, image_url)
+
+    # Upload meme to Cloudinary
+    upload = cloudinary.uploader.upload(
+        "images/{filename}".format(filename=filename)
+    )
+
+    # Respond with downloadable url
+    response = {
+        "response_type": "in_channel",
+        "attachments": [{
+            "image_url": upload['secure_url'],
+            "text": "Here you go"
+        }]
+    }
+    return jsonify(response), 200
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def upload_file():
@@ -47,7 +104,7 @@ def upload_file():
         filename = file.filename
         public_name = request.form.get("public_id")
         if not public_name:
-            #TODO: return error
+            # TODO: return error
             return redirect(request.url)
         # if user does not select file, browser also
         # submit a empty part without filename
@@ -55,13 +112,20 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(filename):
             filetype = filename.rsplit('.', 1)[1]
-            filename_tmp = "{uuid}.{ext}".format(uuid = str(uuid4()), ext = filetype)
-            filepath = os.path.join(UPLOAD_FOLDER, filename_tmp)
+            filename_tmp = "{uuid}.{ext}".format(
+                uuid=str(uuid4()),
+                ext=filetype
+            )
+            filepath = os.path.join(config.UPLOAD_FOLDER, filename_tmp)
             public_id = encode_id(public_name)
 
             file.save(filepath)
-            cloudinary.uploader.upload(filepath, folder = SOURCE_IMAGES_PATH, public_id = public_id)
-            #TODO: show success
+            cloudinary.uploader.upload(
+                filepath,
+                folder=config.SOURCE_IMAGES_PATH,
+                public_id=public_id
+            )
+            # TODO: show success
             return redirect(request.url)
     return '''
     <!doctype html>
@@ -78,53 +142,10 @@ def upload_file():
     '''
 
 
-
 @app.route('/monitor')
 def monitor():
     return 'OK', 200
 
-def list_source_images():
-    images_all_data = cloudinary.api.resources(type = "upload", prefix = SOURCE_IMAGES_PATH)
-    resources = images_all_data["resources"]
-    def formatResource(resource):
-        base_string = "http://res.cloudinary.com/polirytmi/image/upload"
-        quality = "q_60"
-        image_id = "{id}.{format}".format(id = resource["public_id"], format = resource["format"])
-        thumbnail_url = "{base}/{quality}/{image_id}".format(base = base_string, quality = quality, image_id = image_id)
-        name = resource["public_id"].split("/")[-1]
-        return {"title": decode_id(name), "thumb_url": thumbnail_url}
-    return map(formatResource, resources)
-
-def create_meme(name, top_text, bottom_text):
-    source_images = list_source_images()
-    source_image_names = map(lambda i: i.get("title"), source_images)
-    #Check that such an image exists, respond with error if not
-    if name not in source_image_names:
-        image_not_found_text = "The image [{name}] was not found. Here's a list of valid images:".format(name = name)
-        response = {
-            "text": image_not_found_text,
-            "attachments": source_images
-        }
-        return jsonify(response), 200
-    #Find correct download url
-    source_image = source_images[source_image_names.index(name)]
-    image_url = source_image["thumb_url"]
-
-    #Make meme to local file
-    filename = make_meme(top_text, bottom_text, image_url)
-
-    #Upload meme to Cloudinary
-    upload = cloudinary.uploader.upload("images/{filename}".format(filename=filename))
-
-    #Respond with downloadable url
-    response = {
-        "response_type": "in_channel",
-        "attachments": [{
-            "image_url": upload['secure_url'],
-            "text": "Here you go"
-        }]
-    }
-    return jsonify(response), 200
 
 @app.route('/', methods=['POST'])
 def main():
---
+++ config_example.py
@@ -0,0 +1,14 @@
+CLOUDINARY_CONF = {
+    "cloud_name": "foo",
+    "api_key": "12345",
+    "api_secret": "foobar"
+}
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+SERVER_SECRET = "foobar"
+
+HASH_LENGTH = 14
+
+SOURCE_IMAGES_PATH = "source_images"
+UPLOAD_FOLDER = 'source_images'
